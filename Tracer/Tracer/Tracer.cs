cd4468f18cb5a57be31597d43c11450ac9d3e499
Zhecha <j.prejs@yandex.com>
2018-11-09 21:21:48 +0300

    add tracer class

---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class Tracer : ITracer
+    {
+        private TraceResult traceResult;
+
+        public  Tracer()
+        {
+            traceResult = new TraceResult();
+        }
+
+
+        public void StartTrace()
+        {
+            MethodBase baseMethod = new StackTrace().GetFrame(1).GetMethod();
+            var method = new MethodInfo
+            {
+                ClassName = baseMethod.ReflectedType.Name,
+                Name = baseMethod.Name
+            };
+            traceResult.GetThread(Thread.CurrentThread.ManagedThreadId).StartMethodTracing(method);
+
+        }
+
+        public void StopTrace()
+        {
+            traceResult.GetThread(Thread.CurrentThread.ManagedThreadId).StopMethodTracing();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+    }
+}
