fa7dd2f1f8ccaefa09ec77e5ffeae446a9b4dbfb
Zhecha <j.prejs@yandex.com>
2018-11-09 21:25:57 +0300

    add test of tracer

---
+++ Tracer/TracerTest/UnitTestTracer/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTracer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTracer")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b4893470-2c1a-40eb-9bf1-dc1d7cc96c5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tracer/TracerTest/UnitTestTracer/UnitTest1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace UnitTestTracer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class TracerTests
+        {
+            private Tracer.Tracer tracer;
+            int waittime = 200;
+
+            public void TestPattern()
+            {
+                tracer.StartTrace();
+                Thread.Sleep(waittime);
+                tracer.StopTrace();
+            }
+
+
+            [TestMethod]
+            public void OneMethodTimeTest()
+            {
+                tracer = new Tracer.Tracer();
+                TestPattern();
+                long actualtime = tracer.GetTraceResult().Threads[0].Time;
+                long expected = waittime;
+                Assert.IsTrue(actualtime >= expected);
+            }
+
+
+            [TestMethod]
+            public void OneInAnotherTimeTest()
+            {
+                tracer = new Tracer.Tracer();
+                tracer.StartTrace();
+                Thread.Sleep(waittime);
+                TestPattern();
+                tracer.StopTrace();
+                long actualtime = tracer.GetTraceResult().Threads[0].Time;
+                long expected = waittime * 2;
+                Assert.IsTrue(actualtime >= expected);
+            }
+
+
+            [TestMethod]
+            public void MultiThreadTimeTest()
+            {
+                tracer = new Tracer.Tracer();
+                var threadList = new List<Thread>();
+                for (int i = 0; i < 3; i++)
+                {
+                    Thread thread = new Thread(TestPattern);
+                    threadList.Add(thread);
+                    thread.Start();
+                }
+                foreach (Thread thread in threadList)
+                {
+                    thread.Join();
+                }
+                long actualtime = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    actualtime += tracer.GetTraceResult().Threads[i].Time;
+                }
+                long expected = waittime * 3;
+                Assert.IsTrue(actualtime >= expected);
+            }
+
+
+            [TestMethod]
+            public void MultiThreadTest()
+            {
+                tracer = new Tracer.Tracer();
+                var threadList = new List<Thread>();
+                for (int i = 0; i < 4; i++)
+                {
+                    Thread thread = new Thread(TestPattern);
+                    threadList.Add(thread);
+                    thread.Start();
+                }
+                foreach (Thread thread in threadList)
+                {
+                    thread.Join();
+                }
+                Assert.AreEqual(tracer.GetTraceResult().Threads.Count, 4);
+                for (int i = 0; i < 4; i++)
+                {
+                    Assert.AreEqual(tracer.GetTraceResult().Threads[i].InsideMethods.Count, 1);
+                }
+            }
+
+
+            [TestMethod]
+            public void OneInAnotherTest()
+            {
+                tracer = new Tracer.Tracer();
+                tracer.StartTrace();
+                Thread.Sleep(waittime);
+                TestPattern();
+                tracer.StopTrace();
+                Assert.AreEqual(tracer.GetTraceResult().Threads.Count, 1);
+                Assert.AreEqual(tracer.GetTraceResult().Threads[0].InsideMethods.Count, 1);
+                Assert.AreEqual(tracer.GetTraceResult().Threads[0].InsideMethods[0].InsideMethods.Count, 1);
+                Assert.AreEqual(tracer.GetTraceResult().Threads[0].InsideMethods[0].Name, "OneInAnotherTest");
+                Assert.AreEqual(tracer.GetTraceResult().Threads[0].InsideMethods[0].ClassName, "TracerTests");
+                Assert.AreEqual(tracer.GetTraceResult().Threads[0].InsideMethods[0].InsideMethods[0].Name, "TestPattern");
+                Assert.AreEqual(tracer.GetTraceResult().Threads[0].InsideMethods[0].InsideMethods[0].ClassName, "TracerTests");
+            }
+        }
+    }
+}
