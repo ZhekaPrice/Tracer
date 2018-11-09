26a07c57e5b6d0efe7dbd50e729617eb327d4ef2
Zhecha <j.prejs@yandex.com>
2018-11-09 21:23:20 +0300

    add class write in file

---
+++ Tracer/Tracer/Write.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Tracer
+{
+    public class FileWrite : IWrite
+    {
+        private string filename;
+        public  FileWrite(string Filename)
+        {
+            filename = Filename;
+        }
+        public void Write(ISerialize serialize, TraceResult traceResult)
+        {
+            using (var fileStream = new FileStream(filename, FileMode.Create))
+            {
+                serialize.Serialize(fileStream, traceResult);
+            }
+        }
+    }
+
+    public class ConsoleWrite : IWrite
+    {
+        public void Write(ISerialize serialize, TraceResult traceResult)
+        {
+            using (Stream console = Console.OpenStandardOutput())
+            {
+                serialize.Serialize(console,traceResult);
+            }
+        }
+    }
+}
