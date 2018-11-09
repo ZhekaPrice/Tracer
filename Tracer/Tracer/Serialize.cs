84607f475c4e55e9c6276b3255bea27a4a81f1bd
Zhecha <j.prejs@yandex.com>
2018-11-09 21:21:05 +0300

    add serilization

---
+++ Tracer/Tracer/Serialize.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+
+namespace Tracer
+{
+    public class JSONSerialize : ISerialize
+    {
+        public void Serialize(Stream stream, TraceResult traceResult)
+        {
+            var jsonFormat = new DataContractJsonSerializer(typeof(TraceResult));
+
+            using (var jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(stream, Encoding.UTF8, true, true))
+            {
+                jsonFormat.WriteObject(jsonWriter, traceResult);
+            }
+        }
+    }
+
+    public class XmlSerialize : ISerialize
+    {
+        public void Serialize(Stream stream, TraceResult traceResult)
+        {
+            var xmlFormat = new DataContractSerializer(typeof(TraceResult));
+            var setting = new XmlWriterSettings();
+            setting.Indent = true;
+            using (var xmlWriter = XmlWriter.Create(stream, setting))
+            {
+                xmlFormat.WriteObject(xmlWriter, traceResult);
+            }
+
+        }
+    }
+}
